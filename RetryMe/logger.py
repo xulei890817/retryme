24786e5c1ae0351c7f373a3ebe5cdc189a0bd38d
leixu <349106576@qq.com>
2018-04-27 16:35:46 +0800

    Update logger

--- RetryMe/logger.py
+++ RetryMe/logger.py
@@ -12,8 +12,11 @@ import logging
 import sys
 
 import logging
+logger = logging.getLogger("RetryMe")
 
 formatter = logging.Formatter('%(asctime)s %(module)s %(lineno)d %(levelname)-8s: %(message)s')
-logger = logging.StreamHandler(sys.stdout)
-logger.formatter = formatter  # 也可以直接给formatter赋值
-logger.setLevel(logging.WARN)
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.formatter = formatter  # 也可以直接给formatter赋值
+console_handler.setLevel(logging.WARN)
+
+logger.addHandler(console_handler)
